1291933b4104f8436e852f6e365558fe027b0e56
Kanari <hzc14@mails.tsinghua.edu.cn>
2016-08-02 12:42:24 +0800

    Minor fixes

--- src/reversi/ui/BaseController.java
+++ src/reversi/ui/BaseController.java
@@ -92,7 +92,6 @@ public class BaseController {
 		stage.titleProperty().unbind();
 		titleLabel.textProperty().bind(stage.titleProperty());
 
-		__rootPane.widthProperty().addListener(observable -> System.out.println(__rootPane.getParent().getLayoutBounds()));
 		JFXDepthManager.setDepth(toolbar, 1);
 		borderShadows.getChildren().get(1).setEffect(new DropShadow(BlurType.GAUSSIAN, Color.rgb(0, 0, 0, 0.26), 10, 0.12, 2, 0));
 		borderShadows.getChildren().get(2).setEffect(new DropShadow(BlurType.GAUSSIAN, Color.rgb(0, 0, 0, 0.26), 10, 0.12, -2, 0));
